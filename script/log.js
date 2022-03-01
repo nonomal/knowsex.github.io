733077975b8b3d795a1ee8683b48319292d752a0
knowsex <knowsex@outlook.com>
2022-03-01 21:06:07 +0800

    更新联系方式和若干问题。

--- script/log.js
+++ script/log.js
@@ -1,5 +1,9 @@
-console.log("%c┬ ┬┌─┐┬  ┌─┐┌─┐┌┬┐┌─┐  ┌┬┐┌─┐  ┬┌─┌┐┌┌─┐┬ ┬┌─┐┌─┐─┐ ┬ ┌┐┌┌─┐┌┬┐\n│││├┤ │  │  │ ││││├┤    │ │ │  ├┴┐││││ ││││└─┐├┤ ┌┴┬┘ │││├┤  │ \n└┴┘└─┘┴─┘└─┘└─┘┴ ┴└─┘   ┴ └─┘  ┴ ┴┘└┘└─┘└┴┘└─┘└─┘┴ └─o┘└┘└─┘ ┴", 'color:#fd498d;font-size:20px;text-shadow: 0px 0px 10px #FF0000;');
-console.log("%c欢迎来到知性，一个性教育网站。如果你觉得网站代码有什么问题，也欢迎发邮件联系。", 'color:#fff;font-size:20px;text-shadow: 0px 0px 3px #FF0000;font-family:思源黑体,黑体,苹方,hans');
+console.log("%c┬ ┬┌─┐┬  ┌─┐┌─┐┌┬┐┌─┐  ┌┬┐┌─┐  ┬┌─┌┐┌┌─┐┬ ┬┌─┐┌─┐─┐ ┬ ┌┐┌┌─┐┌┬┐"+
+            "\n│││├┤ │  │  │ ││││├┤    │ │ │  ├┴┐││││ ││││└─┐├┤ ┌┴┬┘ │││├┤  │ "+
+            "\n└┴┘└─┘┴─┘└─┘└─┘┴ ┴└─┘   ┴ └─┘  ┴ ┴┘└┘└─┘└┴┘└─┘└─┘┴ └─o┘└┘└─┘ ┴ ", 
+            'color:#fd498d;font-size:20px;text-shadow: 0px 0px 10px #FF0000;');
+console.log("%c欢迎来到知性，一个性教育网站。如果你觉得网站代码有什么问题，也欢迎发邮件联系。",
+            'color:#fff;font-size:20px;text-shadow: 0px 0px 3px #FF0000;font-family:思源黑体,黑体,苹方,hans');
 let nowDate = new Date();
 let startDate = new Date("2021-02-06T00:00:00");
 let founded = Math.floor((nowDate.getTime() - startDate.getTime())/1000/60/60/24)-1;
